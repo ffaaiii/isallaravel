d111328300055fe950f80dab6b77ae27319d6540
unknown <muhammadfaisalnurkarim2@gmail.com>
2025-09-30 14:59:32 +0700

    first commit

---
+++ database/seeders/DepartemenSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DepartemenSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $departemen = [
+            ['kodedepartemen' => '110','nama_departemen' => 'IT'],
+            ['kodedepartemen' => '111','nama_departemen' => 'Logistic'],
+            ['kodedepartemen' => '112','nama_departemen' => 'HR'],
+           
+            
+        ];
+        DB::table('departemen')->insert($departemen);
+    }
+}
